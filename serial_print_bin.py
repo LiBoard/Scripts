7ac477137aae35fe940facd9d89c5784b2d21b5a
Philipp Leclercq <54592274+PhilLecl@users.noreply.github.com>
2021-03-30 00:16:29 +0200

    Cleaned up Python scripts

--- serial_print_bin.py
+++ serial_print_bin.py
@@ -7,13 +7,10 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
+"""Print the binary data from the Arduino as it comes in."""
+
 from serial import Serial
 from bitstring import Bits
-
-
-def is_starting_position(bin_board: Bits):
-    return len(list(bin_board.findall('0b11000011', bytealigned=True))) == 8
-
 
 if __name__ == '__main__':
     with Serial('/dev/ttyACM0') as arduino:
@@ -21,5 +18,4 @@ if __name__ == '__main__':
             if arduino.in_waiting >= 8:
                 data = Bits(arduino.read(8))
                 print(data.bin)
-                print(list(data.findall('0b1')))
                 print()
