a7992afb2143c3347403f3c91086b88c374ddcdc
Philipp Leclercq <54592274+PhilLecl@users.noreply.github.com>
2021-08-23 15:23:34 +0200

    Add args to keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -16,16 +16,17 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import argparse
 
 import chess
 import chess.pgn
-from liboard import LiBoard
-from sys import argv
+import liboard
 import pyautogui
+from liboard import LiBoard
 
 
-def main(_white: bool):
-    board = LiBoard()
+def main(_args: argparse.Namespace):
+    board = LiBoard(_args.port, _args.baud_rate, _args.move_delay)
 
     @board.start_handler
     def print_start(_board: LiBoard) -> bool:
@@ -35,7 +36,7 @@ def main(_white: bool):
     @board.move_handler
     def print_move(_board: LiBoard, _move: chess.Move) -> bool:
         print(_move)
-        if bool(_board.chessboard.ply() % 2) == _white:
+        if bool(_board.chessboard.ply() % 2) != args.side:
             pyautogui.write(str(_move))
         return False
 
@@ -44,8 +45,10 @@ def main(_white: bool):
 
 
 if __name__ == '__main__':
-    white = not (len(argv) > 1 and (argv[1].lower() == 'b' or argv[1].lower() == 'black'))
+    parser = argparse.ArgumentParser(description=__doc__, parents=[liboard.ARGUMENT_PARSER])
+    parser.add_argument('-B', '--black', action='store_const', dest='side', default=0, const=1, help='Play as black')
+    args = parser.parse_args()
     try:
-        main(white)
+        main(args)
     except KeyboardInterrupt:
         pass
