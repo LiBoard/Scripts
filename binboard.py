18cd58ac395359b5e305c3c4b99d135973eaced5
Philipp Leclercq <54592274+PhilLecl@users.noreply.github.com>
2021-08-23 15:23:26 +0200

    Add args to binboard

--- binboard.py
+++ binboard.py
@@ -16,14 +16,23 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import argparse
 
 from bitstring import Bits
 from serial import Serial
 
 if __name__ == '__main__':
-    with Serial('/dev/ttyACM0') as arduino:
-        while True:
-            if arduino.in_waiting >= 8:
-                data = Bits(arduino.read(8))
-                print(data.bin)
-                print()
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-p', '--port', default='/dev/ttyACM0',
+                        help='The serial port which the board is connected to (Default /dev/ttyACM0)')
+    parser.add_argument('-b', '--baud-rate', default=9600, type=int, help='The board\'s baud rate (Default 9600)')
+    args = parser.parse_args()
+    try:
+        with Serial(args.port, args.baud_rate) as arduino:
+            while True:
+                if arduino.in_waiting >= 8:
+                    data = Bits(arduino.read(8))
+                    print(data.bin)
+                    print()
+    except KeyboardInterrupt:
+        pass
